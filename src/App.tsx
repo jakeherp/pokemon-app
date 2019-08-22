cbc1e4f45d0114a355c31f5b5665d672f37c849d
Jacob Herper <jacob@herper.uk>
2019-08-22 16:04:49 +0100

    Apollo Client implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import ApolloClient, { gql } from "apollo-boost"
+import { ApolloProvider } from "@apollo/react-hooks"
+
+const client = new ApolloClient({
+	uri: `https://graphql-pokemon.now.sh`,
+})
+
+client
+	.query({
+		query: gql`
+			{
+				pokemons(first: 151) {
+					name
+					number
+					image
+				}
+			}
+		`,
+	})
+	.then(result => console.log(result))
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<header>
+				<h1>Headline</h1>
+			</header>
+			<main>
+				<ul>
+					<li>Element</li>
+				</ul>
+			</main>
+			<footer>&copy; 2019</footer>
+		</ApolloProvider>
+	)
 }
 
-export default App;
+export default App
